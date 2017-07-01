1b3b5299d2e9fc5d6003f01923e20dc4e066c315
Taemin Kim <taemin3000@naver.com>
2017-07-01 15:20:38 +0900

    put off calendar

---
+++ step02MaxdayMonth/Maxday.java
@@ -0,0 +1,15 @@
+package step02MaxdayMonth;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+/**
+ * Created by taemi on 2017-07-01.
+ */
+public class Maxday {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("달을 입력하세요.");
+
+    }
+}
